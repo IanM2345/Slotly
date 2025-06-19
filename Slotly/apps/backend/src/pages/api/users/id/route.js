295b6707d688ce1ee115a483a9f1a8addb0e1f21
IanM2345 <gachokiian63@gmail.com>
2025-06-19 10:55:46 +0100

    Implemented Users id route.js skeleton

---
+++ Slotly/apps/backend/src/pages/api/users/id/route.js
@@ -0,0 +1,46 @@
+import {NextResponse} from 'next/server';
+import {PrismaClient} from '../../../generated/prisma'; 
+
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+export async function PUT (request, {params}){
+
+    try{
+        const data = await request.json();
+        console.log('Data received for update:', data);
+
+        const {name, email, role} = data;
+
+        const userId = params.id;
+
+        const updatedUser =await prisma.user.update({
+            where: {id},
+            data: {
+                name,
+                email,
+                role
+            },
+    });
+
+    return NextResponse.json(updatedUser);
+}catch (error) {
+    console.error('Error updating user:', error);
+    return NextResponse.json({error: 'Internal Server Error'}, {status: 500});
+  }
+}
+export async function DELETE (request, {params}){
+  try {
+    const userId = params.id;
+
+    const deletedUser = await prisma.user.delete({
+      where: { id: userId },
+    });
+
+    return NextResponse.json(deletedUser);
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
