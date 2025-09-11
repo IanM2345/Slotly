fd8a6834e7a4c81c94fe3fe53230d55998edf8b0
IanM2345 <gachokiian63@gmail.com>
2025-09-11 08:27:10 +0100

    yaay

--- Slotly/apps/mobile/app/business/_layout.tsx
+++
@@ -1,7 +0,0 @@
-// apps/mobile/app/business/_layout.tsx
-"use client";
-import { Stack } from "expo-router";
-
-export default function BusinessLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
